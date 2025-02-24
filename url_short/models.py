5e9e23613c0c18bed1f0c47cc647f4476db479ea
piyushgupta10 <gptpiyush10@gmail.com>
2025-02-24 11:44:12 +0530

    model created

--- url_short/models.py
+++ url_short/models.py
@@ -1,3 +1,18 @@
+import random
+import string
 from django.db import models
 
 # Create your models here.
+
+def generate_short_code():
+    """Generate a random short code for the URL"""
+    return ''.join(string.ascii_letters +string.digits,k=6)
+
+class ShortURL(models.Model):
+    original_Url=models.URLField()
+    short_code=models.CharField(max_length=10,unique=True,default=generate_short_code)
+    created_at=models.DateTimeField(auto_now_add=True)
+    expires_at=models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.original_url}-> {self.short_code}"
